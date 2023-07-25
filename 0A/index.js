be8b46aa6637c96d9a508eaca8e27dc0383d4eb1
Bhoomika <bhoomikahm18@gmail.com>
2023-07-25 10:33:45 +0530

    String Concatenation and Finding Length of the String

--- 0A/index.js
+++ 0A/index.js
@@ -8,11 +8,33 @@ console.log(2001);*/
 //There are six primary data-type in JavaScript(excluding symbol)
 // a) Number b)String c)Boolean d)NULL e)Undefined f)Object
 
-console.log(5);
-console.log(25.69);
+// console.log(5);
+// console.log(25.69);
 
-console.log('JavaScript');
-console.log('My name is Bhoomika');
+// console.log('JavaScript');
+// console.log('My name is Bhoomika');
 
-console.log(5 > 6);
-console.log(5 <= 25);
+// console.log(5 > 6);
+// console.log(5 <= 25);
+
+// var a = null;
+// console.log(a);
+
+// var b;
+// console.log(b);
+
+/*console.log(65 + 55);
+console.log(65 - 55);
+console.log(65 * 55);
+console.log(65 / 55);
+console.log(65 % 55);*/
+
+//String Concatenation
+
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+//String length
+
+console.log('Hello'.length);
+console.log('My name is Anthony Gonsalvis'.length);
