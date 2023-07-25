595a01ea6e7ff454165d68f42c76c89e9d96bb4a
Bhoomika <bhoomikahm18@gmail.com>
2023-07-25 11:04:29 +0530

    Working on different Variables

--- 0A/index.js
+++ 0A/index.js
@@ -8,20 +8,22 @@ console.log(2001);*/
 //There are six primary data-type in JavaScript(excluding symbol)
 // a) Number b)String c)Boolean d)NULL e)Undefined f)Object
 
-// console.log(5);
-// console.log(25.69);
+/*console.log(5);
+console.log(25.69);
 
-// console.log('JavaScript');
-// console.log('My name is Bhoomika');
+console.log('JavaScript');
+console.log('My name is Bhoomika');
 
-// console.log(5 > 6);
-// console.log(5 <= 25);
+console.log(5 > 6);
+console.log(5 <= 25);
 
-// var a = null;
-// console.log(a);
+var a = null;
+console.log(a);
 
-// var b;
-// console.log(b);
+var b;
+console.log(b);*/
+
+//Arthmetic Operators
 
 /*console.log(65 + 55);
 console.log(65 - 55);
@@ -40,7 +42,7 @@ console.log('Hello' + ' ' + 'World');*/
 console.log('My name is Anthony Gonsalvis'.length);*/
 
 //String Methods
-console.log('Hello'.toUpperCase());
+/*console.log('Hello'.toUpperCase());
 console.log('VAnaKKAm'.toLowerCase());
 console.log('Super Ranga'.startsWith('S'));
 console.log('Super Ranga'.startsWith('R'));
@@ -49,4 +51,22 @@ console.log(Math.random());
 console.log(Math.floor(46.7));
 console.log(Math.ceil(46.7));
 console.log(Number.isInteger(75));
-console.log(Number.isInteger(75.33));
+console.log(Number.isInteger(75.33));*/
+
+//Variables
+
+var myName = 'Bhoomi';
+var age = 22;
+
+console.log(myName);
+console.log(age);
+console.log('My name is ' + myName + ' & My age is ' + age);
+
+//Local variables
+
+let myName = 'Kishan';
+let age = 3;
+
+console.log(myName);
+console.log(age);
+console.log('My name is ' + myName + ' & My age is ' + age);
