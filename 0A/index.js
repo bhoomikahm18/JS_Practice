b54d2693db3b114a923ccb6d0c7e453c69add2a0
Bhoomika <bhoomikahm18@gmail.com>
2023-07-25 10:23:00 +0530

    JS examples on Datatypes

--- 0A/index.js
+++ 0A/index.js
@@ -1,2 +1,18 @@
 //JavaScript is a scripting language which was initially used for Client Side VAlidation.
 //Now it is Used for Server Side Programming After the induction of specification ES6.
+
+/*console.log("Hello World");
+console.log("My name is Bhoomika");
+console.log(2001);*/
+
+//There are six primary data-type in JavaScript(excluding symbol)
+// a) Number b)String c)Boolean d)NULL e)Undefined f)Object
+
+console.log(5);
+console.log(25.69);
+
+console.log('JavaScript');
+console.log('My name is Bhoomika');
+
+console.log(5 > 6);
+console.log(5 <= 25);
