1a0023c2ba7672ff00a29c4012d210c0391a27e6
Bhoomika <bhoomikahm18@gmail.com>
2023-07-26 10:01:24 +0530

    Examples on Conditions and Functions in Javascripts

---
+++ 0B/index.js
@@ -0,0 +1,29 @@
+//Conditions and Functions
+
+let flag = true;
+if (flag) {
+    console.log("Salute, Salute, Salute");
+}
+
+if (5 > 6) {
+    console.log("Do Something");
+} else if (7 > 9) {
+    console.log("Do Something Something");
+}
+else {
+    console.log("Dont do anything");
+}
+
+//Non-Boolean inside IF Condition
+//If we are using non-boolean values as a condition in IF:
+//For number:0, For String : '',"",null are considered as FALSE
+//Any other number is considered as TRUE.
+//Any undefined or null or NaN(not a number) will be considered as FALSE.
+
+let flag = '';
+
+if (flag) {
+    console.log("Flag is Truthy");
+} else {
+    console.log("Flag is Falsy");
+}
