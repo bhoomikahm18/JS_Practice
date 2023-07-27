6715b5044b8ebb09c24b68a30444dc72b56c80dd
Bhoomika <bhoomikahm18@gmail.com>
2023-07-27 10:36:17 +0530

    Unshift and shift methods

--- 0C/index.js
+++ 0C/index.js
@@ -40,7 +40,7 @@ let southEastAsia = ['India','Pakistan','Bangladesh','Buthan','Burma','Nepal'];
 console.log(southAsia.length);
 console.log(southEastAsia.length);
 
-//PUSh OR POP
+//PUSh OR POP: methods are used to add and remove element to an array from the end(to the end).
 let nato = ['USA','UK','Germany','Canada','France','Italy','Turkey'];
 
 nato.push('Romania','Greece','Latvia','Lithuania','Estonia');
@@ -65,3 +65,11 @@ console.log(nato);
   'Turkey',  'Romania',
   'Greece'
 ]*/
+
+//UNSHIFT OR SHIFT: methods are used to add and remove element to an array from the Start(to the Start).
+nato.unshift('Ukraine','Finland','Poland');
+console.log(nato);
+
+nato.shift();
+nato.shift();
+console.log(nato);
