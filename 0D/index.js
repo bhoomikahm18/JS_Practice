d5f4c9ff23e601ed4f4e0ba60e4c5607820bf96a
Bhoomika <bhoomikahm18@gmail.com>
2023-07-28 10:30:02 +0530

    Working on Call Back Functions

--- 0D/index.js
+++ 0D/index.js
@@ -11,6 +11,7 @@ addTwoNumbers(5, 6);
 addMethod(5, 6);
 
 //Printing Function Name
+
 console.log(addTwoNumbers);
 console.log(addMethod);
 /*output: [Function: addTwoNumbers]
@@ -20,3 +21,25 @@ console.log(addTwoNumbers.name);
 console.log(addMethod.name);
 /*output: addTwoNumbers
 addTwoNumbers*/
+
+//Call Back Functions(Aam Zindagi)
+const add = (x, y) => {
+    console.log(`Sum of Two Numbers: ${x, y}`);
+}
+
+add(5, 6);
+
+/*const add = function(x, y) {
+    console.log(`Sum of Two Numbers: ${x, y}`);
+}*/
+
+//Call Back Functions(Mentos Zindagi)
+const sum = (param) => {
+    param(5, 6);
+    console.log(`Finished adding two numbers`);
+}
+
+const add = (x, y) => {
+    console.log(`Sum of Two Numbers: ${x + y}`);
+}
+sum(add);
