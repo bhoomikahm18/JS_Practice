8618dc3fbf828db1484bcd39a0852c1efded3201
Bhoomika <bhoomikahm18@gmail.com>
2023-07-29 14:47:58 +0530

    Adding and deleting the Properties

--- 0E/index.js
+++ 0E/index.js
@@ -13,3 +13,29 @@ console.log(`The dog is weight ${dog.weight} Kilos`);
 let response = dog['responds to'];
 console.log(`My dog responds to ${response}`);
 console.log(dog.tag_no);
+
+let baby = {
+    cat : 'kitten',
+    dog : 'puppy',
+    kangaroo : 'zoey',
+    'horse baby' : 'fawn',
+    'baby duck' : 'duckling',
+    'lion' : 'cub',
+    sheep :'lamb'
+}
+
+console.log(baby);
+
+// console.log(baby.cat);
+// console.log(baby.dog);
+// console.log(baby['horse baby']);
+// console.log(baby['baby duck']);
+// console.log(baby.lion);
+
+baby.cow = 'calf';
+baby['Baby Monkey'] = 'Infant';
+
+delete baby.sheep;
+delete baby['baby duck'];
+
+console.log(baby);
