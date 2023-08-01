258dec0ce278548075677f49b38c44e10a320c26
Bhoomika <bhoomikahm18@gmail.com>
2023-08-01 10:39:00 +0530

    Bind function Execution

--- 0F/index.js
+++ 0F/index.js
@@ -120,3 +120,34 @@ printName5.apply(personTwo5, [b, 'Maharastra']);
 //Call and Apply
 //call() mathod is using 'pass by reference' as we pass the object on which the method is to be called(with some additional parameters: comma separator)
 //apply() mathod is using 'pass by reference' as we pass the object on which the method is to be called(with some additional parameters: as an array)
+
+//Bind Method
+//bind() method returns a function, but binds the function to the object & arguments we are passing(and keeps it ready).
+//To invoke it, we have to exclusively use the call() method (if of same line)
+//-or- invoke it by using a parenthesis in the consecutive line, by acceting the return of the function object(returned by the bind method).
+
+let personOne6 = {
+    firstname: 'Balaji',
+    lastname: 'KR',
+
+}
+
+let personTwo6 = {
+    firstname: "Sachin",
+    lastname: "tendulkar",
+
+}
+let printName6 = function (hometown, state) {
+    console.log(`\n ${this.firstname} ${this.lastname} 
+    from ${hometown}, ${state}`);
+}
+
+let f1 = printName6.bind(personOne6, 'Bengaluru', 'Karnataka');
+let f2 = printName6.bind(personTwo6, 'Mumbai');
+
+console.log(f1);
+console.log(f2);
+
+f1();
+f2();
+
