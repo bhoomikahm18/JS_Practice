5feae3c23973eefb0970edcd57ab847f1412d141
Bhoomika <bhoomikahm18@gmail.com>
2023-08-04 11:56:29 +0530

    Calculating the Human Body Temperature

--- 0J/water-limits-es6.js
+++ 0J/water-limits-es6.js
@@ -6,8 +6,9 @@ buttonElement.addEventListener('click', clickMe);
 function clickMe() {
     let element = document.getElementById("container");
     element.innerHTML = `<h2>Boiling Point is: ${celsiusToFahrenheit(100)}
-    & Freezing Point is: ${celsiusToFahrenheit(0)}</h2>`;
+    & Freezing Point is: ${celsiusToFahrenheit(0)}</h2>
+    <h2>Human Body Temerature is: ${fahrenheitToCesius(98.6)} Degree Celsius<h2>`;
 }
 
 console.log(`Water Boiling Point is ${celsiusToFahrenheit(100)} Degree Fahrenheit`);
-// console.log(`Human Body temperature is ${fahrenheitToCesius(98.6)} Degree celsius`);
+console.log(`Human Body temperature is ${fahrenheitToCesius(98.6)} Degree celsius`);
