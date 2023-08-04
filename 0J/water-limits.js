03d286c69e3da42dffb8e2fc56b4800a6d3884c0
Bhoomika <bhoomikahm18@gmail.com>
2023-08-04 11:13:09 +0530

    Example on Importing Modules

--- 0J/water-limits.js
+++ 0J/water-limits.js
@@ -2,10 +2,14 @@
 //     return celsius * (9 / 5) + 32;
 // }
 
-const converters = require('./converters.js');
+// const converters = require('./converters.js');
+const { celsiusToFahrenheit } = require('./converters.js');
 
-const freezingPointF = converters.celsiusToFahrenheit(0);
-const boilingPointF = converters.celsiusToFahrenheit(100);
+// const freezingPointF = converters.celsiusToFahrenheit(0);
+// const boilingPointF = converters.celsiusToFahrenheit(100);
+
+const freezingPointF = celsiusToFahrenheit(0);
+const boilingPointF = celsiusToFahrenheit(100);
 
 console.log(`The freezing point of water in Fahrenhiet is ${freezingPointF}`);
 console.log(`The boiling Point of water in Fahrenhiet is ${boilingPointF}`);
