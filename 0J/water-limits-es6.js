a5f76832f19dd38ae54b9eed85cd12a6298be7fd
Bhoomika <bhoomikahm18@gmail.com>
2023-08-04 11:28:40 +0530

    Creating JS file

---
+++ 0J/water-limits-es6.js
@@ -0,0 +1,12 @@
+import { celsiusToFahrenheit, fahrenheitToCesius } from "./converters-es6.js";
+
+const buttonElement = document.getElementById('button');
+buttonElement.addEventListener('click', clickMe);
+
+function clickMe() {
+    let element = document.getElementById("container");
+    element.innerHTML = `<h2>Boiling Point is: ${celsiusToFahrenheit(100)}
+    & Freezing Point is: ${celsiusToFahrenheit(0)}</h2>`;
+}
+
+console.log(`Water Boiling Point is ${celsiusToFahrenheit(100)} Degree Fahrenheit`);
