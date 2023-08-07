309f81adcff6687fbea49ec6531c7b559e4dac23
Bhoomika <bhoomikahm18@gmail.com>
2023-08-07 10:21:10 +0530

    Playing with setTimeOut

--- 0K/index.js
+++ 0K/index.js
@@ -14,7 +14,7 @@ balajiBath();*/
 
 //Mentos Zindagi
 
-let balajiBath = function () {
+/*let balajiBath = function () {
     console.log('\nBalaji Woke Up');
     console.log('Balaji is Bathing');
     console.log('Balaji is Ready');
@@ -26,4 +26,20 @@ let kishanBath = function (signalHim) {
 }
 
 kishanBath();
-setTimeout(balajiBath, 7000);
+setTimeout(balajiBath, 7000);*/
+
+//Using Callbacks(Ensuring Happening)
+let balajiBath = function () {
+    console.log('\nBalaji Woke Up');
+    console.log('Balaji is Bathing');
+    console.log('Balaji is Ready');
+}
+let kishanBath = function (signalHim) {
+    setTimeout(() => { console.log('\nKishan Woke Up') }, 2000);
+    setTimeout(() => { console.log('Kishan is Bathing') }, 3000);
+    setTimeout(() => { console.log('Kishan is Ready') }, 5000);
+
+    setTimeout(signalHim, 7000);
+}
+
+kishanBath(balajiBath);
