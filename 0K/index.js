bf1b9cfd23fb9588dd7e10d27f61b5529ee35322
Bhoomika <bhoomikahm18@gmail.com>
2023-08-07 10:10:11 +0530

    Working on Callback functions

---
+++ 0K/index.js
@@ -0,0 +1,13 @@
+let balajiBath = function () {
+    console.log('\nBalaji Woke Up');
+    console.log('Balaji is Bathing');
+    console.log('Balaji is Ready');
+}
+let kishanBath = function () {
+    console.log('\nKishan Woke Up');
+    console.log('Kishan is Bathing');
+    console.log('Kishan is Ready');
+}
+
+kishanBath();
+balajiBath();
