1a795c1af21ad3bbfd1baf3712ebdd721d920923
Bhoomika <bhoomikahm18@gmail.com>
2023-08-14 10:26:03 +0530

    Events and JSON

---
+++ 0M/index.js
@@ -0,0 +1,7 @@
+//Events and JSON
+/* -> String: It is a representation of a information in character set.
+Eg: "Hello world" //string
+'name=Amar id=101 score=90' //String
+
+let str = '{name: 'Amar', id: 101, score: 90}' //String JSON
+*/
