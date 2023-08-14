a0ac892bd78aafbd66de60f0b45c9ce9b073a7c3
Bhoomika <bhoomikahm18@gmail.com>
2023-08-14 10:37:44 +0530

    Structure and Style

--- 0M/index.js
+++ 0M/index.js
@@ -7,7 +7,8 @@ let str = '{name: 'Amar', id: 101, score: 90}' //String JSON
 */
 
 //JavaScript Object Notation
-/*The above code will run but if we try to access values using dot operator it says undefined
+/*The above code will run but if we try to access values using dot operator it says undefined.
+Variable 'str' s a plain string.
 */
 let str = '{name: Amar, id: 101, score: 90}';
 console.log(str.id);
