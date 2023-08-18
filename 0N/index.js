b931d8c18413d41d1bfe43e9882506afc4d2da02
Bhoomika <bhoomikahm18@gmail.com>
2023-08-18 10:57:35 +0530

    Working on Prototypes Inheritance

--- 0N/index.js
+++ 0N/index.js
@@ -30,10 +30,11 @@ let longEar = {
     __proto__: rabbit
 };
 
+
 let animal1 = {
     eats: true,
     walk() {
-       /*This method won't be used by rabbit*/
+        /*This method won't be used by rabbit*/
     }
 };
 
@@ -41,6 +42,33 @@ let rabbit1 = {
     __proto__: animal
 };
 
-rabbit1.walk = function() {
+rabbit1.walk = function () {
     alert("Rabbit! Bounce-bounce!");
 };
+
+let user = {
+    name: "John",
+    surname: "Smith",
+
+    set fullName(value) {
+        [this.name, this.surname] = value.split(" ");
+    },
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    }
+
+};
+let admin = {
+    __proto__: user,
+    isAdmin: true
+};
+/*Output:
+alert(admin.fullName);
+undefined
+admin.fullName = "Alice Cooper";  
+'Alice Cooper'
+alert(admin.fullName);
+undefined
+alert(user.fullName);
+undefined
+*/
